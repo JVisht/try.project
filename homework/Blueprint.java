eb9f380902449ec2eac44d60c93bbae759374074
JAYSHREE VISHT <106261614+JVisht@users.noreply.github.com>
2022-05-28 12:13:14 -0400

    Blueprint , mainclass

---
+++ homework/Blueprint.java
@@ -0,0 +1,31 @@
+package homework;
+
+public class Blueprint {
+
+   void addition (int num1 , int num2){
+       int total = num1 + num2;
+       System.out.println( total);
+}
+    int subtraction (int num1, int num2){
+       int total = num1 - num2;
+         return total;
+
+   }
+    void multiplication (){
+        int num1 =5;
+        int num2 = 10;
+        int total = num1 * num2;
+        System.out.println( total);
+    }
+    String person2(String name, int age, boolean maritalStatus){
+        System.out.println("name: "+name);
+        System.out.println("age: "+age);
+        System.out.println("married: "+maritalStatus);
+        return name;
+    }
+
+
+
+
+}
+
---
+++ homework/mainclass.java
@@ -0,0 +1,20 @@
+package homework;
+
+public class mainclass {
+    public static void main(String[] args) {
+        Blueprint cal  = new Blueprint();
+      cal.addition(10 ,5 );
+
+        System.out.println(cal.subtraction(40 ,30));
+
+
+     Blueprint yourself = new Blueprint();
+     yourself.person2("Max" ,30, true);
+
+    }
+
+
+
+
+
+}
